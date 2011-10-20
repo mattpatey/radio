80a9326f511be0fa5a1913cf419a7444c0efdf80
Matt Patey <malp@moire.(none)>
2011-10-21 00:18:46 +0200

    Fixed a test.
    Added a new test.

--- radio/tests/radio_tests.py
+++ radio/tests/radio_tests.py
@@ -1,10 +1,14 @@
 
+import os
+
 from tempfile import (
     mkdtemp,
     NamedTemporaryFile,
     )
 
-from radio.util import get_oldest_file
+from time import sleep
+
+from radio import util
 
 
 def test_get_oldest_file():
@@ -13,5 +17,18 @@ def test_get_oldest_file():
     """
     tmp_folder = mkdtemp()
     oldest_file = NamedTemporaryFile(dir=tmp_folder)
+    sleep(1)
+    kinda_new_file = NamedTemporaryFile(dir=tmp_folder)
+    sleep(1)
+    newest_file = NamedTemporaryFile(dir=tmp_folder)
+    assert util.get_oldest_file(tmp_folder) == oldest_file.name
 
-    assert get_oldest_file(tmp_folder) == oldest_file.name
+def test_make_file_ancient():
+    """
+    make_file_ancient should set a file's atime to the beginning of
+    the epoch.
+    """
+    tmp_folder = mkdtemp()
+    tmp_file = NamedTemporaryFile(dir=tmp_folder)
+    util.make_file_ancient(tmp_file.name)
+    assert os.stat(tmp_file.name).st_atime == 0.0
--- radio/util.py
+++ radio/util.py
@@ -3,8 +3,18 @@ import os
 
 
 def get_oldest_file(path):
+    """
+    Return file with the smallest atime in a path.
+    """
     files = [os.path.join(path, f) for f in os.listdir(path)]
     files_with_atime = [(f, os.stat(f).st_atime) for f in files]
     files_sorted_by_atime = sorted(files_with_atime, key=lambda x: x[1])
 
     return files_sorted_by_atime[0][0]
+
+def make_file_ancient(path):
+    """
+    Set a file's atime to beginning of the Unix epoch.
+    """
+    ancient_times = (0, 0)
+    os.utime(path, ancient_times)
